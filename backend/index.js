2370f19b319ddc543def9231678070dbf8245b70
Pradeep Maurya <pm053208@gmail.com>
2025-08-05 23:01:25 +0530

    all

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 const path = require('path');
 
 const corsOptions = {
-  origin: 'https://neon-kheer-16199f.netlify.app/', // Allow your frontend origin
+  origin: 'https://neon-kheer-16199f.netlify.app', // Allow your frontend origin
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allow these methods
   credentials: true,
   optionsSuccessStatus: 204 // For legacy browser support
