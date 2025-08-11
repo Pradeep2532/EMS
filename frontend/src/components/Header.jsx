edf301e085b5dce8ba017913cd204e0c07149c0c
Pradeep Maurya <pm053208@gmail.com>
2025-08-11 07:58:24 +0530

    responsive

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='bg-emerald-600 text-white flex justify-between items-center px-5 py-5 text-sm sm:text-xl'>
+    <div className='header bg-emerald-600 text-white flex justify-between items-center px-5 py-5 text-sm sm:text-xl'>
       <h1 className='text-base sm:text-2xl'>Employee Management System</h1>
-      <div className='flex space-x-4 text-sm sm:text-lg'>
+      <div className='text flex space-x-4 text-sm sm:text-lg'>
         <Link to="/" className='hover:text-emerald-300'>All Employees</Link>
         <Link to="/add-employee" className='hover:text-emerald-300'>Add Employee</Link>
       </div>
